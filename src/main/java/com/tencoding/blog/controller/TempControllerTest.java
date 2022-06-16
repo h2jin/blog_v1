e84d2cf85e5df68443a239edbcda88724b7997dd
h2jin <102580742+h2jin@users.noreply.github.com>
2022-06-16 10:14:09 +0900

    yml 파일 추가 설정
    jpa 설정 추가 jsp 설정 추가

---
+++ src/main/java/com/tencoding/blog/controller/TempControllerTest.java
@@ -0,0 +1,18 @@
+package com.tencoding.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TempControllerTest {
+	
+	// http://localhost:9090/blog/temp/home
+	@GetMapping("/temp/home")
+	// 파일 리턴 경로 : src/main/resource/static (기본)
+	// 리턴명 : /home.html
+	// 
+	public String temphome() {
+		System.out.println("tempHome()");
+		return "/home.html"; // 슬러시를 꼭 넣어줘야지 찾을 수 있다.
+	}
+}
