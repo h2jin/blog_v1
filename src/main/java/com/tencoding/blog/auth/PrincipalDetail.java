3dc0fde84e0d75b030b3e2d0d240d2c58a85b0e5
h2jin <102580742+h2jin@users.noreply.github.com>
2022-06-27 17:48:06 +0900

    serialVersionUID

--- src/main/java/com/tencoding/blog/auth/PrincipalDetail.java
+++ src/main/java/com/tencoding/blog/auth/PrincipalDetail.java
@@ -14,11 +14,17 @@ import lombok.Data;
 public class PrincipalDetail implements UserDetails {
 
 	/**
-	 * 
+	 *  serialVersionUID -> 직렬화 할 때 사용하는 것.
+	 *  모든 클래스는 UID(고유 식별자) 를 가지고 있음.
+	 *  Class의 내용이 변경되면 UID 값 역시 같이 변경된다.
+	 *  직렬화 하여 통신하고 UID 값으로 통신한게 정상인지 확인을 하는데,
+	 *  그 값이 바뀌게 되면 다른 class로 인식을 해버리게 된다.
+	 *  이를 방지하기 위해서 고유값으로 미리 명시를 해주는 부분이 serialVersionUID 인 것.
+	 *  이를 명시하지 않으면 알 수없는 오류 발생할 수 있기 때문에 권장사항!
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private User user;
+	private User user; // 컴포지션 관계
 	
 
 	public PrincipalDetail(User user) {
